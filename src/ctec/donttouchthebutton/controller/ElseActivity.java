71f4f14e1c28c62d9e261610fad959c307705426
SirConanD <fetzegra000@my.jordandistrict.org>
2014-11-19 09:15:05 -0700

    Added to repo and changed settings

---
+++ src/ctec/donttouchthebutton/controller/ElseActivity.java
@@ -0,0 +1,17 @@
+package ctec.donttouchthebutton.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ElseActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_else);
+	}
+}
